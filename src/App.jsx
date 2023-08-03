c6d7a99da56c82269549fb8411a466cde7ee278c
AmadeuVerdeiro <amadeuverdeiro@gmail.com>
2023-08-03 10:06:56 +0200

    feat: fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { getCityData } from './API/API';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+  const [coordData, setCoordData] = useState(0);
+  
+ useEffect(() => {
+    setCity("Palencia");
+    setCountry("ES");
+ },[]);
 
+  
+  useEffect(()=> {
+   setCoordData(getCityData());
+  },[]);
+
+/*   const dataWeather = API_GET_WEATHER(coordData.lat, coordData.lon);
+  
+  console.log(dataWeather);
+   */
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    
+  </>
+  )}
 
 export default App
